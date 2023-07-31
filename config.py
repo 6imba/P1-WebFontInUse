2044bddac51cc8cfd8e3c6820ef927377dbba1f2
6imba <simbaamir55@gmail.com>
2023-07-31 08:32:05 +0545

    `initial-commit`

---
+++ config.py
@@ -0,0 +1,13 @@
+# file constant:
+FONT_DIR = "C:/Users/DELL/Desktop/Font Detial/WebFontInUse/fonts"
+STATE_JSON = "C:/Users/DELL/Desktop/Font Detial/WebFontInUse/files/state.json"
+ERROR_LOG = "C:/Users/DELL/Desktop/Font Detial/WebFontInUse/log/error.log"
+SKIP_URL = "C:/Users/DELL/Desktop/Font Detial/WebFontInUse/log/skipped_url.log"
+SITE_FONT_URLS = "C:/Users/DELL/Desktop/Font Detial/WebFontInUse/files/site_font_urls.csv"
+
+# database constant:
+DB_NAME = "devubas_fontinuse2"
+HOST_NAME = "dev.uba-solutions.com"
+USER_NAME = "devubas_fontuser"
+PASSWORD = "?cH9NUf+Qn{~"
+
